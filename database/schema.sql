0bd2893442de26d23fc991c0e3e666534f9f401c
Ryan Hikel <hikelryan@gmail.com>
2018-06-01 10:54:43 -0400

    created regions plants and join table

--- database/schema.sql
+++ database/schema.sql
@@ -1,12 +1,24 @@
-CREATE DATABASE dogstagram;
-
-\c dogstagram
-
-DROP TABLE IF EXISTS dogs;
-
-CREATE TABLE dogs
+DROP DATABASE plants_db;
+CREATE DATABASE plants_db;
+\c plants_db
+DROP TABLE IF EXISTS plants;
+DROP TABLE IF EXISTS regions;
+CREATE TABLE regions
+(
+    region_id SERIAL PRIMARY KEY,
+    region_name TEXT NOT NULL
+);
+CREATE TABLE plants
+(
+    plant_id SERIAL PRIMARY KEY,
+    plant_name TEXT NOT NULL,
+    species TEXT NOT NULL,
+    tree_img_url TEXT NOT NULL,
+    leaf_img_url TEXT NOT NULL
+);
+CREATE TABLE plant_region
 (
     id SERIAL PRIMARY KEY,
-    breed TEXT,
-    name TEXT
+    plant_id INTEGER REFERENCES plants (plant_id),
+    region_id INTEGER REFERENCES regions (region_id)
 );
